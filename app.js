79f91b92d109d0bb4f6ffb813e0f20031b3cd6c8
raindropcity <raindropcity0209@gmail.com>
2021-12-09 22:02:39 +0800

    add create function

--- app.js
+++ app.js
@@ -4,8 +4,25 @@ const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
 const app = express()
 
+// 載入list.js
+const List = require('./models/list')
+
 // Define localhost ralated variables
 const port = 3000
+
+// 載入Mongoose
+const mongoose = require('mongoose')
+// 定義資料庫連線狀態
+const dataBase = mongoose.connection
+mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+dataBase.on('error', () => {
+  console.log('MongoDB error')
+})
+
+dataBase.once('open', () => {
+  console.log('MongoDB connected')
+})
 
 // 告訴Express模板引擎要使用Handlebars
 // app.engine()解釋：定義要使用的樣板引擎。第一個參數是這個樣板引擎的名稱(就是副檔名)；第二個參數是放入和此樣板引擎相關的設定。這裡設定了預設的佈局(default layout)需使用名為main的檔案。 所以Express會去抓main.handlebars這支檔案。
@@ -13,14 +30,19 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 // 透過app.set()告訴Express說要設定的view engine是handlebars
 app.set('view engine', 'handlebars')
 
-// 靜態檔案所在的資料夾
+// 靜態檔案所在的資料夾 & 使用body-parser所提供的解析URL方法
 // 告訴 Express 靜態檔案是放在名為 public 的資料夾中，它不必針對這個資料夾內的檔案做什麼，只要產生對應的路由讓我們可以使用就好
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 // 設定路由
 app.get('/', (req, res) => {
-  // res.render()是指Express會「回傳HTML來呈現前端樣板」
-  res.render('index', { restaurants: restaurantList.results })
+  List.find().lean().then((lists) => {
+    // res.render()是指Express會「回傳HTML來呈現前端樣板」
+    res.render('index', { lists: lists })
+  })
+    .catch((error) => { console.log(error) })
+
 })
 
 app.get('/search', (req, res) => {
@@ -29,6 +51,11 @@ app.get('/search', (req, res) => {
     return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword)
   })
   res.render('index', { restaurants: restaurants, keyword: keyword })
+
+  // const restaurants = List.find().filter(restaurant => {
+  //   return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword)
+  // })
+  // res.render('index', { restaurants: restaurants, keyword: keyword })
 })
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
@@ -37,6 +64,28 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
   })
 
   res.render('show', { restaurantList: restaurant[0] })
+})
+
+// 新增一筆資料
+app.get('/lists/new', (req, res) => {
+  res.render('new')
+})
+
+app.post('/lists', (req, res) => {
+  const body = req.body
+  return List.create({
+    name: body.name,
+    name_en: body.nameEn,
+    category: body.category,
+    image: body.image,
+    location: body.location,
+    phone: body.phone,
+    google_map: body.googleMap,
+    rating: body.rating,
+    description: body.description
+  })
+    .then(() => { res.redirect('/') })
+    .catch((error) => { console.log(error) })
 })
 
 // 監聽並啟動伺服器
